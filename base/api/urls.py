0aec2a8e16ceccc8a5d71c2b87a61c840d48dd75
Sagar Darade <daradesagar.1998@gmail.com>
2022-11-11 12:22:29 +0530

    Added REST points

--- base/api/urls.py
+++ base/api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.getRoute),
+    path('rooms/',views.getRooms),
+    path('room/<str:pk>/',views.getRoom),
 ]
--- studybud/settings.py
+++ studybud/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # 
     'base',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
